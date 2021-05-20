a44cdaede16fdcdaa83420c281fa0f979a0bf2d0
ronbaheti <70448874+ronbaheti@users.noreply.github.com>
2021-05-20 18:58:35 -0400

    Adding comments

--- Map Generation/MoonShine_Visualization.m
+++ Map Generation/MoonShine_Visualization.m
@@ -1,15 +1,19 @@
+% Ron Baheti
+% 04.29.2021
+
 function[] = MoonShine_Visualization(solfilename, mapfilename, ~)
 
-%read in map and solution
-x = load(solfilename);
-map = load(mapfilename);
+% Read in map and solution
+x = load(solfilename); %solfilename is a solution file created in the build folder once MS is run
+map = load(mapfilename); %mapfilename is the map text file created using the map generator code
 
-%Plot path on map
-
+% Plot map
 figure(1);
 
 contourf(map);
 hold on;
+
+% Plot path on map
 
 h = text(x(1,1), x(1,2), 'START');
 set(h,'LineWidth',5);
